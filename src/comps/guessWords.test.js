57fd8628f752e75226572aa7840ca05cc34f3a03
Obo Agboghidi <oboagboghidi@Obos-MacBook-Pro-2.local>
2020-04-22 15:10:21 -0400

    Start test for guessword

---
+++ src/comps/guessWords.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {shallow} from 'enzyme'
+import {findByTestAttr, checkProps} from '../../test/testUtils';
+
+import GuessWords from './guessWords.js'
+
+const defaultProps =  {
+    guessedWords: [ {guessedWords: 'train', letterMatchCount: 3}],
+};
+
+/**
+ * Factory function to create a shallowWrapper or the App Component
+ * @function setup
+ * @param {object} props - Component props specific to this setup
+ * @param {object} state - Initial state for setup
+ * @returns {ShallowWrapper}
+ */
+
+const setup = (props={}) => {
+    const setupProps = {...defaultProps, ...props }
+    return shallow(<GuessWords { ...setupProps } />)
+}
+
+test('does not throw warning with expected warning', () => {
+    checkProps(GuessWords, defaultProps)
+})
+
+describe('if there are no words guessed', () => {
+    let wrapper
+    beforeEach(() => {
+     wrapper = setup({ guessedWords: []})
+    })
+    test('renders without error', () => {
+        const component = findByTestAttr(wrapper, 'component-guessed-words')
+        expect(component.length).toBe(1)
+    })
+    test('renders instructions to guess a word', () => {
+        const instructions = findByTestAttr(wrapper, 'guess-instructions')
+        expect(instructions.text().length).not.toBe(0)
+    })
+})
+describe('if there are words guessed', () => {
+
+})
